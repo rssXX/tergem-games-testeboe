621e129adfea1f81dee47858d17326ebcabd616b
Romanov S <rss00@yandex.ru>
2024-03-05 16:48:38 +0500

    Работает подкл файлов

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
-import pkg from 'pg';
-const { Client } = pkg;
+import http from 'http'
+import fs from 'fs'
+import path from "path"
+import { fileURLToPath } from 'url';
 
-const createTableUsers = async () => {
-    const client = new Client({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'target',
-        password: '',
-        port: 5432,
-    })
-    await client.connect();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-
-
-    let createTableQuery = `
-        CREATE TABLE IF NOT EXISTS users(
-            id INTEGER PRIMARY KEY NOT NULL,
-            username VARCHAR(50) NOT NULL,
-            email VARCHAR(50) NOT NULL,
-            createAt INTEGER NOT NULL,
-            status VARCHAR(3) NOT NULL
-        );
-    `;
-
-    await client.query(createTableQuery)
-    console.log("Создана таблица");
-    await client.end()
+const readFileAsync = async (path) => {
+    return new Promise((resolve, reject) => fs.readFile(path, {encoding: "utf-8"}, (err, data) => {
+        if (err){
+            reject(err.message)
+        }
+        resolve(data)
+    }))
 }
 
-createTableUsers()
+http.createServer(async (request, response) => {
+    let data;
+    let type;
+
+    if (request.url === '/') {
+        data = await readFileAsync(path.join(__dirname, 'index.html'));
+        type = 'text/html'
+    }
+
+    if (request.url === '/client/index.css') {
+        data = await readFileAsync(path.join(__dirname, 'client', 'index.css'));
+        type = 'text/css'
+    }
+
+    if (request.url === '/client/script/main.js') {
+        data = await readFileAsync(path.join(__dirname, 'client', 'script', 'main.js'));
+        type = 'text/js'
+    }
+
+    if(data){
+        response.writeHead(200, {'Content-Type': type});
+        response.write(data);
+        response.end();
+    }else{
+        response.writeHead(404, {'Content-Type': "text/plain"});
+        response.write("Error 404: Recourse not found!");
+        response.end();
+    }
+
+}).listen(3000, () => {
+    console.log('Start http://localhost:3000/');
+});
---
+++ server/db.js
@@ -0,0 +1,31 @@
+import pkg from 'pg';
+const { Client } = pkg;
+
+const createTableUsers = async () => {
+    const client = new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'target',
+        password: '',
+        port: 5432,
+    })
+    await client.connect();
+
+
+
+    let createTableQuery = `
+        CREATE TABLE IF NOT EXISTS users(
+            id INTEGER PRIMARY KEY NOT NULL,
+            username VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            createAt INTEGER NOT NULL,
+            status VARCHAR(3) NOT NULL
+        );
+    `;
+
+    await client.query(createTableQuery)
+    console.log("Создана таблица");
+    await client.end()
+}
+
+createTableUsers()
